99ed28c0922fbd1506e41b13a8c87999bd9475f9
ouuxxxi <1018743511@qq.com>
2019-10-30 14:13:17 +0800

    打印9*9乘法表

---
+++ Practice5.java
@@ -0,0 +1,13 @@
+public class Practice5 {
+    public static void main(String[] args) {
+        int i=0;
+        for(i=1;i<=9;i++)
+        {
+            int j =0;
+            for(j=1;j<=i;j++)
+            {
+                System.out.println(i+"*"+j+"="+(i*j));
+            }
+        }
+    }
+}
