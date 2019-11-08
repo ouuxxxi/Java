a36442269b46aab86cef6cfdf81e402750f178d0
ouuxxxi <1018743511@qq.com>
2019-11-08 20:56:01 +0800

    检验一个数能否被5和6整除； 检验一个数能否被5或6整除； 检验一个数能否被5或6整除但不能被同时整除；

---
+++ shiyan2.java
@@ -0,0 +1,12 @@
+package TestBooleanOperators;
+
+public class TestBooleanOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 Scanner input = new Scanner(System.in);
+	 System.out.print
+
+	}
+
+}
