51742accf22a8e35aa083be55999ea33719a6652
ouuxxxi <1018743511@qq.com>
2019-11-10 17:51:02 +0800

    产生5个随机加法问题，加数是0到15之间的整数，显示正确答案的个数和测验时间

---
+++ shiyan3.java
@@ -0,0 +1,43 @@
+package AdditionQuizLoop;
+
+import java.util.Scanner;
+
+public class AdditionQuizLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int NUMBER_OF_QUESTIONGS = 10;
+		int correctCount = 0;
+		int count = 0;
+		long startTime = System.currentTimeMillis();
+		String output = " ";
+		Scanner input = new Scanner(System.in);
+		while (count < NUMBER_OF_QUESTIONGS) {
+			int number1 = (int)(Math.random()*15);
+			int number2 = (int)(Math.random()*15);
+		
+		
+		System.out.print(
+				"What is" + number1 + "+" + number2 + " ? ");
+		int answer = input.nextInt();
+		
+		if(number1 + number2 == answer){
+			System.out.println("You are correct");
+			correctCount++;
+		}
+		else {
+			System.out.println("You are wrong. \n" + number1 + "+" + number2 + "should be"+ (number1 + number2));
+		    count++;
+		
+		output += "\n" + number1 + "+" + number2 + "=" + answer +((number1 + number2 == answer)? "correct" : "wrong");
+		}   
+		
+		long endTime = System.currentTimeMillis();
+		long testTime = endTime - startTime;
+		
+		System.out.println ("Correct count is" + correctCount + "\nTest time is " + testTime / 1000 + "second\n"+ output);
+		
+	}
+
+	}
+}
