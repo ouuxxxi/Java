a19ea700ebbadf53a0dff6a4e571a6e3ce578f27
ouuxxxi <1018743511@qq.com>
2019-12-02 21:46:30 +0800

    字符串的连接

---
+++ PracticeString.java
@@ -0,0 +1,23 @@
+//字符串的连接
+/*
+import java.util.*;
+public class PracticeString {
+    public static String func(String str) {
+        String[] strs = str.split(" ");
+        String ret = "";
+        //for(String s : strs) {
+       //   }
+
+        for (int i = 0; i <strs.length;i++)
+        ret += strs[i];
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();//abc def
+        String ret = func(str);
+        System.out.println(ret);
+    }
+}
+*/
