4da6b97fd2a0108ded4a37a9acbb2498576a8bf0
ouuxxxi <1018743511@qq.com>
2019-12-18 19:39:30 +0800

    /

---
+++ practice11.java
@@ -0,0 +1,27 @@
+// 获取一个数二进制序列中所有的偶数位和奇数位， 分别输出二进制序列。
+
+public class practice11 {
+public  sattic int func1(int n){
+        int count = 0;
+        for(int i =0;i<=32;i++){
+        if((n>>i)&1==1){
+            count++;
+            System.out.println("奇数的个数为：");
+            else if((n>>i)&1==0)){
+                System.out.println("偶数的个数为：");
+            }
+        }
+        }
+
+
+
+        }
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
