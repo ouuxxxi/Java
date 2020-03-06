2b731308061e548ea680d81930427b511877aed0
ouuxxxi <1018743511@qq.com>
2020-03-06 14:51:38 +0800

    -

---
+++ 蓝桥杯：01字符串.java
@@ -0,0 +1,18 @@
+package com.java.lanqiaobei;
+// 01字串:
+public class Main10 {
+    public static void main(String[] args) {
+        for (int i = 0; i <2 ; i++) {
+            for (int j = 0; j <2 ; j++) {
+                for (int k = 0; k <2 ; k++) {
+                    for (int l = 0; l <2 ; l++) {
+                        for (int m = 0; m < 2; m++) {
+                            System.out.print(i+""+j+""+""+k+""+l+""+m);
+                            System.out.println();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
